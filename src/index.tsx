c15999249d1ce3805b6a2b56fd41c69bf4bb1bf0
Igboanugwo Collins <munisco12@gmail.com>
2021-04-04 06:00:06 +0100

    Added header

--- src/index.tsx
+++ src/index.tsx
@@ -6,11 +6,13 @@ import {Provider} from 'react-redux'
 import thunk from 'redux-thunk'
 import { reducers } from './store/reducers/reducer'
 import { App } from './components/App'
+import Header from './components/Layout/Header'
 
 const store = createStore(reducers, applyMiddleware(thunk))
 
 const app = (
     <Provider store={store}>
+        <Header/>
         <App/>
     </Provider>
 )
