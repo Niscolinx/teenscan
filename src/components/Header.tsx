ad2c11e5a2e77e6ab92904d2f0ed43273a37e462
Igboanugwo Collins <munisco12@gmail.com>
2021-04-05 10:23:49 +0100

    styling the header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,11 +6,13 @@ const Header = () => {
     return (
         <>
             <img src={Logo} alt="logo"/>
+            <Link to='/'>Categories</Link>
             <div className="input-box">
             <BiSearch/>
             <input className='header_input'/>
             </div>
             <Link to='/'>Courses</Link>
+            <Link to='/'>Teach on Teenscanblog</Link>
 
             <ul className='header_list'>
                 <li className="header_list-item"><Link to='/'>Login</Link></li>
