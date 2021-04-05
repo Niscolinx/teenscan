ee9fae3f97f46f222b02f9efa941679f6ef6fc53
Igboanugwo Collins <munisco12@gmail.com>
2021-04-05 13:25:33 +0100

    styling the header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,11 +2,22 @@ import Logo from '../assets/Logo.png'
 import {BiSearch} from 'react-icons/bi'
 import {Link} from 'react-router-dom'
 
+const categoryModal = (
+    <div>
+        <ul className='categoryModal'>
+            <li>Hello</li>
+            <li>World</li>
+            <li>How</li>
+        </ul>
+    </div>
+)
+
 const Header = () => {
     return (
         <>
             <img src={Logo} alt="logo" style={{marginRight: '2rem'}}/>
             <Link to='/'>Categories</Link>
+            {categoryModal}
 
             <div className="header_input-box">
             <BiSearch className='header_input-box_searchIcon'/>
@@ -17,8 +28,8 @@ const Header = () => {
             <Link to='/'>Teach on Teenscanblog</Link>
 
             <ul className='header_list'>
-                <li className="header_list-item"><Link to='/'>Login</Link></li>
-                <li className="header_list-item"><Link to='/'>Signup</Link></li>
+                <li><Link to='/'  className="header_list-item--1">Login</Link></li>
+                <li><Link to='/'  className="header_list-item--2">Signup</Link></li>
             </ul>
         </>
     )
