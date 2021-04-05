3fb95518f74bbb8918ddb0fe94b63c8bc54437e4
Igboanugwo Collins <munisco12@gmail.com>
2021-04-05 13:31:57 +0100

    styling the category modla

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import {BiSearch} from 'react-icons/bi'
 import {Link} from 'react-router-dom'
 
 const categoryModal = (
-    <div>
+    <div className='category'>
         <ul className='categoryModal'>
             <li>Hello</li>
             <li>World</li>
@@ -16,8 +16,11 @@ const Header = () => {
     return (
         <>
             <img src={Logo} alt="logo" style={{marginRight: '2rem'}}/>
+            <div className="modal">
             <Link to='/'>Categories</Link>
             {categoryModal}
+
+            </div>
 
             <div className="header_input-box">
             <BiSearch className='header_input-box_searchIcon'/>
