c34bcccc4f1a4c7119071e9d2dcd9d2b44f83f61
Igboanugwo Collins <munisco12@gmail.com>
2021-04-05 09:02:04 +0100

    resolved some typescript issues

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,14 @@
 import Logo from '../assets/Logo.png'
+import {BiSearch} from 'react-icons/bi'
 
 const Header = () => {
     return (
         <div>
             <img src={Logo} alt="logo"/>
+            <div className="input-box">
+            <BiSearch/>
             <input className='subHeader_input'/>
+            </div>
         </div>
     )
 }
