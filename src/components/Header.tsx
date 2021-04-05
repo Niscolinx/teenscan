84f1665a966b4445c4a602d7551a583ee8cc3cc7
Igboanugwo Collins <munisco12@gmail.com>
2021-04-05 11:46:28 +0100

    styling the header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,12 +5,14 @@ import {Link} from 'react-router-dom'
 const Header = () => {
     return (
         <>
-            <img src={Logo} alt="logo"/>
+            <img src={Logo} alt="logo" style={{marginRight: '2rem'}}/>
             <Link to='/'>Categories</Link>
+
             <div className="header_input-box">
-            <BiSearch className='header_input-box_search'/>
-            <input className='header_input-box_input'/>
+            <BiSearch className='header_input-box_searchIcon'/>
+            <input className='header_input-box_input' placeholder='Search courses'/>
             </div>
+
             <Link to='/'>Courses</Link>
             <Link to='/'>Teach on Teenscanblog</Link>
 
