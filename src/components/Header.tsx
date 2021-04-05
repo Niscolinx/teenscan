fb23044f7dbe5b3172514da0509b60a2c31fbe15
Igboanugwo Collins <munisco12@gmail.com>
2021-04-05 20:43:25 +0100

    styling the category modal

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,37 +1,62 @@
+import React, {useState} from 'react'
 import Logo from '../assets/Logo.png'
 import {BiSearch} from 'react-icons/bi'
 import {Link} from 'react-router-dom'
 
-let categoryModalStyle = {
-    display: `block`
-}
 
-const categoryModal = (
-    <div className='category' style={categoryModalStyle}>
-        <ul className='categoryModal'>
-            <li>Hello</li>
-            <li>World</li>
-            <li>How</li>
-        </ul>
-    </div>
-)
 
-const handleModalToggle = () => {
-    console.log('mouse entered!')
-   let transFormed = {...categoryModal, display: 'block'}
-   console.log(transFormed)
-   categoryModalStyle = transFormed
-   
-}
 
 const Header = () => {
+    const [state, setState] = useState('none')
+    const [mouseMove, setMouseMove] = useState(false)
+
+    let categoryModalStyle = {
+        display: state
+    }
+    
+    
+    const handleMouseLeave = () => {
+       // setState('none')
+    }
+    const handleMouseEnter = () => {
+        console.log('mouse entered!')
+        setState('block')
+    }
+
+    const handleModalEnter = () => {
+        console.log('modal entered')
+        setMouseMove(true)
+    }
+
+    const handleModalLeave = () => {
+        console.log('modal close')
+        setMouseMove(false)
+       // setState('none')
+    }
+    const categoryModal = (
+        <div className='category' style={categoryModalStyle} onMouseEnter={handleModalEnter}>
+            <ul className='categoryModal' onMouseLeave={handleModalLeave}>
+                 <li><Link to='/'  className="header_list-item--1">Login</Link></li>
+                <li><Link to='/'  className="header_list-item--2">Signup</Link></li>
+                 <li><Link to='/'  className="header_list-item--1">Login</Link></li>
+                <li><Link to='/'  className="header_list-item--2">Signup</Link></li>
+                 <li><Link to='/'  className="header_list-item--1">Login</Link></li>
+                <li><Link to='/'  className="header_list-item--2">Signup</Link></li>
+                <li>Hello</li>
+                <li>World</li>
+                <li>How</li>
+            </ul>
+        </div>
+    )
+
+
     console.log(categoryModalStyle)
     return (
         <>
             <img src={Logo} alt="logo" style={{marginRight: '2rem'}}/>
             
-            <div className="modal">
-            <Link to='/' onMouseOver={handleModalToggle}>Categories</Link>
+            <div className="modal" onMouseLeave={handleMouseLeave} style={{background: 'red'}}>
+            <Link to='/' onMouseOver={handleMouseEnter}>Categories</Link>
             {categoryModal}
             </div>
 
