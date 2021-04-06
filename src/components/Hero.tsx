1c6505cc197f0d9d098452e87bfe5a68a0c863d7
Igboanugwo Collins <munisco12@gmail.com>
2021-04-06 09:30:46 +0100

    styling the hero section

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,10 +1,10 @@
-import hero from '../assets/hero.jpg'
+import hero from '../assets/hero4.jpg'
 
 const Hero = () => {
     return (
-        <>
+        <div className='hero'>
             <img src={hero} alt='hero' className='hero_img' />
-        </>
+        </div>
     )
 }
 
