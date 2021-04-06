eba9141f393a0a2441223e26d6c749bdda76bf66
Igboanugwo Collins <munisco12@gmail.com>
2021-04-06 11:00:48 +0100

    styling the hero section

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,8 +2,10 @@ import hero from '../assets/hero4.jpg'
 
 const Hero = () => {
     return (
-        <div className='hero'>
-            <img src={hero} alt='hero' className='hero_img' />
+        <div className='hero' style={{background: `url(${hero})`, height: '100vh', backgroundSize: 'cover', backgroundRepeat: 'no-repeat'}}>
+            <h1>Dream Up</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci natus et ipsam, odio veritatis accusantium at, maiores rem ea quod enim soluta quibusdam totam dignissimos quaerat. Cum temporibus doloribus nulla!</p>
+         {/* <img src={hero} alt='hero' className='hero_img' /> */}
         </div>
     )
 }
