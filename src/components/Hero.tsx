fdc09cbb02cec94e79eebdf9e4caf0a173ef51a9
Igboanugwo Collins <munisco12@gmail.com>
2021-04-07 12:58:49 +0100

    styling the hero section

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,7 +2,7 @@ import hero from '../assets/hero4.jpg'
 
 const Hero = () => {
     return (
-        <div className='hero' style={{background: `url(${hero})`, height: '100vh', backgroundSize: 'cover', backgroundRepeat: 'no-repeat'}}>
+        <div className='hero'>
             <div className="hero_box">
             <h1>Dream Up</h1>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci natus et ipsam, odio veritatis accusantium at, maiores rem ea quod enim soluta quibusdam totam dignissimos quaerat. Cum temporibus doloribus nulla!</p>
