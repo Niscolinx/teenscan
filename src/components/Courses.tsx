474b403ef286bf775628a386593bffa5dfdbf9db
Igboanugwo Collins <munisco12@gmail.com>
2021-04-07 15:05:21 +0100

    building the card

--- src/components/Courses.tsx
+++ src/components/Courses.tsx
@@ -1,7 +1,16 @@
+import Card from "./card"
+
 const Courses = () => {
     return (
         <>
-           <h1 className='courses_heading '>List of courses</h1> 
+        <div className="courses">
+           <h1 className='courses_heading'>Collection of courses</h1> 
+           <p className='courses_text'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corrupti vitae</p>
+
+        </div>
+        <div className="courses">
+            <Card/>
+        </div>
         </>
     )
 }
