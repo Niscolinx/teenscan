2793cf50c06beddb141cd767a61145997d4068b2
Igboanugwo Collins <munisco12@gmail.com>
2021-04-07 14:17:53 +0100

    building the courses section

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,7 +1,7 @@
 import Courses from '../components/Courses'
 import Hero from '../components/Hero'
 
-const Home = ():JSX.Element => {
+const Home = () => {
     return (
         <>
             <div className='section-hero'><Hero/> </div>
