a38eab9ad524a13ac715ea8dbf6eeebd10805e9c
Igboanugwo Collins <munisco12@gmail.com>
2021-04-07 13:20:50 +0100

    building the courses section

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,9 +1,11 @@
+import Courses from '../components/Courses'
 import Hero from '../components/Hero'
 
 const Home = () => {
     return (
         <>
             <div className='section-hero'><Hero/> </div>
+            <div className="section-courses"><Courses/></div>
         </>
     )
 }
