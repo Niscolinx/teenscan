9f5c481df00403735b9f0bb8808ef0c968fcf057
Igboanugwo Collins <munisco12@gmail.com>
2021-04-07 15:54:27 +0100

    building the course card

--- src/components/Courses.tsx
+++ src/components/Courses.tsx
@@ -6,11 +6,11 @@ const Courses = () => {
         <>
         <div>
            <h1 className='courses_heading'>Collection of courses</h1> 
-           <p className='courses_text'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corrupti vitae</p>
+           <p className='courses_subHeading'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corrupti vitae</p>
 
         </div>
-        <div className="courses_cards">
-            <Course title='Title of th course' creator='John Doe' price={10000} image={cardImg}/>
+        <div className="course_cards">
+            <Course title='Title of the course' creator='John Doe' price={10000} image={cardImg}/>
         </div>
         </>
     )
