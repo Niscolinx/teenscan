f01904815c0a60857bd7b907b1734a56587eff61
Igboanugwo Collins <munisco12@gmail.com>
2021-06-04 14:57:00 +0100

    working on hero svg

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,14 +3,17 @@ import HeroSvg from '../assets/hero.svg'
 const Hero = () => {
     return (
         <div className='hero'>
-            <div className='hero_box'>
-                <h1 className='hero_box--title'>Dream Up</h1>
+            {/* <div className='hero_box'>
+                 <h1 className='hero_box--title'>Dream Up</h1>
                 <p className='hero_box--text'>
                     Lorem ipsum dolor sit amet, consectetur adipisicing elit.
                     Adipisci natus et ipsam, odio veritatis accusantium at,
                     maiores rem ea quod enim soluta quibusdam totam dignissimos
                     quaerat. Cum temporibus doloribus nulla!
-                </p>
+                </p> 
+
+            </div> */}
+            <div className='hero_box'>
 
             </div>
             <img src={HeroSvg} width='100%' height='100%' alt=''/>
