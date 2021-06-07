5ce957c21630f2e55becb7f0a66e7b38001ea2f3
Igboanugwo Collins <munisco12@gmail.com>
2021-06-07 11:22:08 +0100

    working on the header

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,23 +1,39 @@
 import HeroSvg from '../assets/herowave.svg'
+import ReactTypingEffect from 'react-typing-effect'
 
 const Hero = () => {
     return (
         <div className='hero'>
             <div className='hero__box'>
-                 <h1 className='hero__box--title'>Dream Up</h1>
-                <p className='hero__box--text'>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                    Adipisci natus et ipsam, odio veritatis accusantium at,
-                    maiores rem ea quod enim soluta quibusdam totam dignissimos
-                    quaerat.poribus doloribus nulla!
-                </p> 
-
+                <div>
+                    <h1 className=''>
+                        Learn your way with <span>TeensCanBlog</span> to{' '}
+                        <ReactTypingEffect
+                            text={[
+                                'Web Development',
+                                'Blogging',
+                                'Forex',
+                                'Data Analysis',
+                                'Public Speaking',
+                                'Graphic Design',
+                                'Digital Marketing',
+                            ]}
+                            className='text-blue-900'
+                            speed={30}
+                            eraseSpeed={30}
+                            eraseDelay={3000}
+                        />
+                    </h1>{' '}
+                    <p className='hero__box--text'>
+                        Explore your creativity with classes in Web Data Analysis,
+                        Web Development, design, and more.
+                    </p>
+                </div>
             </div>
             <div className='hero__svg'>
-            <img src={HeroSvg} width='100%' height='auto' alt=''/>
+                <img src={HeroSvg} width='100%' height='auto' alt='' />
             </div>
         </div>
-
     )
 }
 
