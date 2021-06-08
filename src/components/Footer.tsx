8229fbc3a05390cbbda491b215898b16f5f23458
Igboanugwo Collins <munisco12@gmail.com>
2021-06-08 20:41:27 +0100

    working on footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,12 +1,26 @@
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
+import { HiOutlineCollection, HiOutlineUserGroup, HiMail } from 'react-icons/hi'
+import { BiHome } from 'react-icons/bi'
+import { GrContact } from 'react-icons/gr'
+
 
 const Footer = () => {
     return (
         <div className='footer'>
             This is the footer
-
-            <ul className="footer__mobileNav">
-                <Link to='/'>Home</Link>
+            <ul className='footer__mobileNav'>
+                <Link to='/'>
+                    <HiOutlineCollection />
+                </Link>
+                <Link to='/'>
+                    <BiHome />
+                </Link>
+                <Link to='/'>
+                    <GrContact />
+                </Link>
+                <Link to='/'>
+                    <HiOutlineUserGroup />
+                </Link>
             </ul>
         </div>
     )
