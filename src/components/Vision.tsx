c18b7466e409f0773bb45a4945b24d71a0e0a661
Igboanugwo Collins <munisco12@gmail.com>
2021-06-10 07:07:20 +0100

    working on footer

---
+++ src/components/Vision.tsx
@@ -0,0 +1,10 @@
+const Vision = () => {
+
+    return <div>
+
+        This is the vision page
+    </div>
+}
+
+
+export default Vision
--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,12 +1,14 @@
 import Courses from '../components/Courses'
 import Hero from '../components/Hero'
+import Vision from '../components/Vision'
 
 const Home = () => {
     return (
         <>
             <div className='section-hero'><Hero/> </div>
-            <div className="section-courses"><Courses/></div>
-        </>
+            <div className="section-courses"><Courses /></div>
+            <div className='section-vision'><Vision/></div>
+        </Vision>
     )
 }
 
