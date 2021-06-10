6c20b2684c38674ddcf5c9184bac9f0484a865f7
Igboanugwo Collins <munisco12@gmail.com>
2021-06-10 18:58:04 +0100

    working on courses

--- src/components/Courses.tsx
+++ src/components/Courses.tsx
@@ -9,10 +9,9 @@ const Courses = () => {
     return (
         <>
             <div className='courses__title--box'>
-                <h1 className='courses__heading'>Collection of courses</h1>
+                <h1 className='courses__heading'>Recent courses</h1>
                 <p className='courses__subHeading'>
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                    Corrupti vitae
+                    Learn the latest skills to reach your professional goals.
                 </p>
             </div>
             <div className='course__cards'>
