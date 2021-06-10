c40298800471a051827cc484608f3badd18689b8
Igboanugwo Collins <munisco12@gmail.com>
2021-06-10 07:19:17 +0100

    working on vision

--- src/components/Vision.tsx
+++ src/components/Vision.tsx
@@ -1,10 +1,13 @@
+import Teens2 from '../assets/teens2.png'
 const Vision = () => {
-
-    return <div>
-
-        This is the vision page
-    </div>
+    return (
+        <div className='vision'>
+            <div>
+                <img src={Teens2} width='100%' height='100%' alt='' />
+            </div>
+            <div></div>
+        </div>
+    )
 }
-
 
 export default Vision
