1ae2a47a8cd6349764fecf85d3195a820b99f6d8
Igboanugwo Collins <munisco12@gmail.com>
2021-06-10 11:17:33 +0100

    working on bookus

---
+++ src/components/BookUs.tsx
@@ -0,0 +1,42 @@
+import Teens3 from '../assets/teens3.png'
+
+
+const BookUs = () => {
+    return (
+        <div className='BookUs = ()'>
+            <div className='BookUs = ()__1'>
+                <img src={Teens3} width='100%' height='100%' alt='' />
+            </div>
+            <div className='BookUs = ()__2'>
+                <div className='BookUs = ()__2--item'>
+                    <h2>The Future Belongs to Tech Minds</h2>
+                    <p>
+                        Teens Can Blog is a tech initiative targeted at
+                        equipping young people with digital exploratory skills.
+                        To become socially relevant and build the nation.
+                    </p>
+                </div>
+                <div className='BookUs = ()__2--item'>
+                    {' '}
+                    <h2>Innovative Ideas</h2>
+                    <p>
+                        We believe that the future will be managed and
+                        controlled by Tech experts. This is why we’re keen at
+                        equipping the young people today with on demand Tech
+                        skills to man their generation.
+                    </p>
+                </div>
+                <div className='BookUs = ()__2--item'>
+                    {' '}
+                    <h2>BookUs = ()</h2>
+                    <p>
+                        To build a Nigerian community filled with independent
+                        young Techprenuers.
+                    </p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default BookUs
