053e768dc882b6e22a2ac9ed0c0ebde824d51a90
Igboanugwo Collins <munisco12@gmail.com>
2021-06-10 11:40:12 +0100

    working on bookus

--- src/components/BookUs.tsx
+++ src/components/BookUs.tsx
@@ -1,38 +1,26 @@
 import Teens4 from '../assets/teens4.jpeg'
+import { FaLongArrowAltRight } from 'react-icons/fa'
 
 
 const BookUs = () => {
     return (
         <div className='bookUs'>
-            <div className='bookUs__2'>
-                <div className='bookUs__2--item'>
-                    <h2>The Future Belongs to Tech Minds</h2>
-                    <p>
-                        Teens Can Blog is a tech initiative targeted at
-                        equipping young people with digital exploratory skills.
-                        To become socially relevant and build the nation.
-                    </p>
-                </div>
-                <div className='bookUs__2--item'>
-                    {' '}
-                    <h2>Innovative Ideas</h2>
-                    <p>
-                        We believe that the future will be managed and
-                        controlled by Tech experts. This is why we’re keen at
-                        equipping the young people today with on demand Tech
-                        skills to man their generation.
-                    </p>
-                </div>
-                <div className='bookUs__2--item'>
-                    {' '}
-                    <h2>bookUs</h2>
-                    <p>
-                        To build a Nigerian community filled with independent
-                        young Techprenuers.
-                    </p>
+            <div className='bookUs__1'>
+                <div className='bookUs__1--item'>
+                    <h1>
+                        We also provide tech training and mentorship, campus
+                        trainings, community projects etc.
+                    </h1>
+
+                    <button className='btn bookUs__btn'>
+                        Book Us{' '}
+                        <span>
+                            <FaLongArrowAltRight />
+                        </span>
+                    </button>
                 </div>
             </div>
-            <div className='bookUs__1'>
+            <div className='bookUs__2'>
                 <img src={Teens4} width='100%' height='100%' alt='' />
             </div>
         </div>
