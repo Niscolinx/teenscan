0c5ece12a11a73aadc10173f17ea4027a0a60c10
Igboanugwo Collins <munisco12@gmail.com>
2021-06-10 20:08:25 +0100

    Added scrdollable

--- src/components/Scrollable.tsx
+++ src/components/Scrollable.tsx
@@ -1,7 +1,7 @@
 const Scrollable = (props: any) => {
     return (
         <div className='scrollable'>
-            {props.children}
+            <div className='scrollable__item'>{props.children}</div>
             <div className='scrollable__overflowShadow'></div>
         </div>
     )
