98525167ddf7fb87c14c571d366e0ce38baa0de2
Igboanugwo Collins <munisco12@gmail.com>
2021-06-12 18:26:30 +0100

    working on about

--- src/views/About.tsx
+++ src/views/About.tsx
@@ -1,16 +1,15 @@
 import Teens3 from '../assets/teens3.png'
 
 const About = () => {
-    return <div className='about'>
-
-        <header>
-            <img src={Teens3} alt=''/>
-        </header>
-
-        <h1>About Us</h1>
-        This is the about page
-         
+    return (
+        <div className='about'>
+            <header>
+                <img src={Teens3} alt='' />
+                <h1>About Us</h1>
+            </header>
+            This is the about page
         </div>
+    )
 }
 
 export default About
