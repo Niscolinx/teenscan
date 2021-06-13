87c3b48edd71f4ffbd4313e7ce265c5040208240
Igboanugwo Collins <munisco12@gmail.com>
2021-06-13 15:48:15 +0100

    working on story section

--- src/views/About.tsx
+++ src/views/About.tsx
@@ -1,4 +1,5 @@
 import Teens3 from '../assets/teens3.png'
+import Story from '../components/Story'
 
 const About = () => {
     return (
@@ -6,7 +7,7 @@ const About = () => {
             <header>
                 <h1>About Us</h1>
             </header>
-            This is the about page
+            <Story/>
         </div>
     )
 }
