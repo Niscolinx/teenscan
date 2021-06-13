45fa9fed3a5dca1bdccef587558ae4cb3d9da640
Igboanugwo Collins <munisco12@gmail.com>
2021-06-13 23:07:55 +0100

    Added video

--- src/components/StoryPrimary.tsx
+++ src/components/StoryPrimary.tsx
@@ -5,32 +5,30 @@ const StoryPrimary = () => {
     return (
         <div className='storyPrimary'>
             <div className='storyPrimary__description'>
-                    <div className='storyPrimary__description--item'>
-                        <h2>The Future Belongs to Tech Minds</h2>
-                        <p>
-                            Teens Can Blog is a tech initiative targeted at
-                            equipping young people with digital exploratory
-                            skills. To become socially relevant and build the
-                            nation.
-                        </p>
-                    </div>
-                    <div className='storyPrimary__description--item'>
-                        {' '}
-                        <h2>Innovative Ideas</h2>
-                        <p>
-                            We believe that the future will be managed and
-                            controlled by Tech experts. This is why we’re keen
-                            at equipping the young people today with on demand
-                            Tech skills to man their generation.
-                        </p>
-                    </div>
-                    <div className='storyPrimary__description--item'>
-                        {' '}
-                        <h2>Vision</h2>
-                        <p>
-                            To build a Nigerian community filled with
-                            independent young Techprenuers.
-                        </p>
+                <div className='storyPrimary__description--item'>
+                    <h2>Our Core Value</h2>
+                    <p>
+                        We believe that the future will be managed and
+                        controlled by Tech experts. This is why we’re keen at
+                        equipping the young people today with on demand Tech
+                        skills to man their generation.
+                    </p>
+                </div>
+                <div className='storyPrimary__description--item'>
+                    {' '}
+                    <h2>Vision</h2>
+                    <p>
+                        To build a Nigerian community filled with independent
+                        young Techprenuers.
+                    </p>
+                </div>
+                <div className='storyPrimary__description--item'>
+                    {' '}
+                    <h2>Mission</h2>
+                    <p>
+                        Equipping the next generation with digital explorational
+                        skill.
+                    </p>
                 </div>
             </div>
             <div className='storyPrimary__video'>
