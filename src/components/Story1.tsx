88da9efad4b2089c65190c48e78dceba8d1f9aa9
Igboanugwo Collins <munisco12@gmail.com>
2021-06-13 21:39:19 +0100

    working on story section

--- src/components/Story1.tsx
+++ src/components/Story1.tsx
@@ -6,17 +6,13 @@ const Story = () => {
         <div className='story'>
             <div className='story__1'>
                 <h1>Who We Are</h1>
+            </div>
 
-              
-            </div>
-         
-            <div className='bg-video'>
-                <video className='bg-video__content' autoPlay muted loop>
-                    {/* <source src={TcbVideo} type='video/mp4' /> */}
-                 <source src={TcbVideoWebm} type="video/webm"/> 
-                    Your browser is not supported
-                </video>
-            </div>
+            <video className='story__2' autoPlay muted loop>
+                <source src={TcbVideo} type='video/mp4' />
+                <source src={TcbVideoWebm} type='video/webm' />
+                Your browser is not supported
+            </video>
         </div>
     )
 }
