e8ba514ab8413f074077c264ae1136fe5f33c52a
Igboanugwo Collins <munisco12@gmail.com>
2021-06-13 20:32:05 +0100

    working on story section

--- src/components/Story.tsx
+++ src/components/Story.tsx
@@ -1,24 +1,28 @@
 import Teens4 from '../assets/teens3.png'
-import { FaLongArrowAltRight } from 'react-icons/fa'
 const Story = () => {
 
     return (
-
-
         <div className='story'>
             <div className='story__1'>
                 <div className='story__1--item'>
                     <h1>
-                        We also provide tech training and mentorship, campus
-                        trainings, community projects etc.
+                        Who We Are
                     </h1>
 
-                    <button className='btn story__btn'>
-                        Get In Touch{' '}
-                        <span>
-                            <FaLongArrowAltRight />
-                        </span>
-                    </button>
+                    <p>
+                        We are a registered business enterprise that is
+                        passionately committed to spurring technological
+                        innovation in young people , formed by a carefully
+                        curated team of Tech Experts, web developers, engineers
+                        and interaction designers with a focus on developing the
+                        next generation with digital explorational skills.
+                        Equipping young people in tertiary institutions, high
+                        schools and local communities with on-demand Tech skills
+                        and mentoring them till become experts and become
+                        socially relevant by integrating their passion with
+                        technology. To build the entrepreneurship mindset in
+                        them, so they become enthusiastic about critical
+                    </p>
                 </div>
             </div>
             <div className='story__2'>
