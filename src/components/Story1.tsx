098ed862cd3003741ff8929d689754884faac7ec
Igboanugwo Collins <munisco12@gmail.com>
2021-06-13 22:40:42 +0100

    Added video

--- src/components/Story1.tsx
+++ src/components/Story1.tsx
@@ -3,16 +3,20 @@ import TcbVideoWebm from '../assets/tcbvideo.webm'
 
 const Story = () => {
     return (
-        <div className='story'>
-            <div className='story__2'>
-                <video autoPlay muted loop className='story__2--video'>
+        <div className='story1'>
+            <div className='story1__2'>
+                <video autoPlay muted loop className='story1__2--video'>
                     <source src={TcbVideo} type='video/mp4' />
                     <source src={TcbVideoWebm} type='video/webm' />
                     Your browser is not supported
                 </video>
             </div>
-            <div className='story__1'>
-                <h1>Who We Are</h1>
+            <div className='story1__1'>
+                <h1>
+                    Teens Can Blog is a tech initiative targeted at equipping
+                    young people with digital exploratory skills. To become
+                    socially relevant and build the nation.
+                </h1>
             </div>
         </div>
     )
