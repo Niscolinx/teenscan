a4f86ba93f0074af359c76956625d6e8190fdd5a
Igboanugwo Collins <munisco12@gmail.com>
2021-06-18 00:24:20 +0100

    working on courses

--- src/views/Checkout.tsx
+++ src/views/Checkout.tsx
@@ -3,18 +3,20 @@ const Checkout = () => {
         <div className='checkout'>
             <h3>Checkout</h3>
 
-            <div>
-                <label className='container'>
-                    One
-                    <input type='checkbox' checked />
-                    <span className='checkmark'></span>
-                </label>
+            <select className='checkout__select'>
+                <option value='localPay'>Local Deposit</option>
+                <option value='Paystack'>Card Payment</option>
+            </select>
 
-                <label className='container'>
-                    Two
-                    <input type='checkbox' />
-                    <span className='checkmark'></span>
-                </label>
+            <div className='checkout__details'>
+                <h3>Order details</h3>
+
+                <div className='checkout__details--items'>
+                    <h3>Title of the course</h3>
+                    <h3>N30,000</h3>
+                </div>
+
+                <button className='button'>Complete Payment</button>
             </div>
         </div>
     )
