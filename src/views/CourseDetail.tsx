f2c9ab6a5cb8b5d803ed46a63aafd33b3d620f48
Igboanugwo Collins <munisco12@gmail.com>
2021-06-17 22:43:56 +0100

    working on courses

--- src/views/CourseDetail.tsx
+++ src/views/CourseDetail.tsx
@@ -20,56 +20,60 @@ const courseDetail = (props: any) => {
 
     return (
         <div className='courseDetail'>
-            <div className='courseDetail__2'>
-                <img src={image} alt='' />
-                <h3>{price}</h3>
-                
-                <button>Buy now</button>
-            </div>
-            <div className='courseDetail__1'>
-                <h2>{title}</h2>
-                <p>{description}</p>
-                <p> Created by {creator}</p>
-            </div>
-            <div>
-                <h3>What you'll learn</h3>
+            <div className='courseDetail__primary'>
+                <div className='courseDetail__2'>
+                    <img src={image} alt='' />
+                    <h3>{price}</h3>
 
-                <ul>
-                    {objectives &&
-                        objectives.map((item: any, index: number) => {
-                            return <li key={index}>{item}</li>
-                        })}
-                </ul>
+                    <button>Buy now</button>
+                </div>
+                <div className='courseDetail__1'>
+                    <h2>{title}</h2>
+                    <p>{description}</p>
+                    <p> Created by {creator}</p>
+                </div>
             </div>
-            <div>
-                <h3>Course Content</h3>
+            <div className='courseDetail__secondary'>
+                <div>
+                    <h3>What you'll learn</h3>
 
-                <ul>
-                    {overview &&
-                        overview.map((item: any, index: number) => {
-                            return <li key={index}>{item}</li>
-                        })}
-                </ul>
-            </div>
-            <div>
-                <h3>Requirements</h3>
+                    <ul>
+                        {objectives &&
+                            objectives.map((item: any, index: number) => {
+                                return <li key={index}>{item}</li>
+                            })}
+                    </ul>
+                </div>
+                <div>
+                    <h3>Course Content</h3>
 
-                <ul>
-                    {requirements &&
-                        requirements.map((item: any, index: number) => {
-                            return <li key={index}>{item}</li>
-                        })}
-                </ul>
-            </div>
-            <div>
-                <h3>This course includes</h3>
+                    <ul>
+                        {overview &&
+                            overview.map((item: any, index: number) => {
+                                return <li key={index}>{item}</li>
+                            })}
+                    </ul>
+                </div>
+                <div>
+                    <h3>Requirements</h3>
 
-                <ul>
-                    {includes &&
-                        includes.map((item: any, index: number) => {
-                            return <li key={index}>{item}</li>
-                        })}
-                </ul>
+                    <ul>
+                        {requirements &&
+                            requirements.map((item: any, index: number) => {
+                                return <li key={index}>{item}</li>
+                            })}
+                    </ul>
+                </div>
+                <div>
+                    <h3>This course includes</h3>
+
+                    <ul>
+                        {includes &&
+                            includes.map((item: any, index: number) => {
+                                return <li key={index}>{item}</li>
+                            })}
+                    </ul>
+                </div>
             </div>
         </div>
     )
