903e1fb191002c4625a2e1b14177c22d1a65e8c9
Igboanugwo Collins <munisco12@gmail.com>
2021-06-18 14:01:36 +0100

    Adding paystack

--- src/views/Checkout.tsx
+++ src/views/Checkout.tsx
@@ -1,126 +1,93 @@
-// const Checkout = (props: any) => {
-//     console.log('checkout props', props.location.state)
-//     const { title, price } = props.location.state
-
-//     const formattedPrice = price.toLocaleString()
-
-//     return (
-//         <div className='checkout'>
-//             <h3 className='checkout__header'>Checkout</h3>
-
-//             <select className='checkout__select'>
-//                 <option value='localPay'>Local Deposit</option>
-//                 <option value='Paystack'>Card Payment</option>
-//             </select>
-
-//             <div className='checkout__paymentType'>
-//                 <h4>Account Details</h4>
-
-//                 <div className='checkout__paymentType--items'>
-//                     <div>
-//                         <span>Bank Name:</span>
-//                         <p className='checkout__paymentType--item'>
-//                             Gurantee Trust Bank
-//                         </p>
-//                     </div>
-//                     <div>
-//                         <span>Account Name:</span>
-//                         <p className='checkout__paymentType--item'>
-//                             Teens Can Blog Tech Comm.
-//                         </p>
-//                     </div>
-//                     <div>
-//                         <span>Account Number:</span>
-//                         <p className='checkout__paymentType--item'>
-//                             0632650272
-//                         </p>
-//                     </div>
-//                 </div>
-//             </div>
-
-//             <div className='checkout__details'>
-//                 <h4>Summary</h4>
-
-//                 <div className='checkout__details--items'>
-//                     <div>
-//                         <span>Course:</span>
-//                         <p className='checkout__details--item'>{title}</p>
-//                     </div>
-//                     <div>
-//                         <span>Price:</span>
-//                         <p className='checkout__details--item'>
-//                             ₦{formattedPrice}{' '}
-//                         </p>
-//                     </div>
-//                 </div>
-
-//                 <button className='button checkout__btn'>
-//                     Complete Payment
-//                 </button>
-//             </div>
-//         </div>
-//     )
-// }
-
-// export default Checkout
+import { PaystackButton } from 'react-paystack'
+const Checkout = (props: any) => {
+    console.log('checkout props', props.location.state)
+    const { title, price } = props.location.state
 
 
-  import { usePaystackPayment } from 'react-paystack'
+const config = {
+    email: 'teenscanblog@gmail.com',
+    amount: 20000,
+    publicKey: 'pk_test_93e3bae833c60377335c8a9e9ade423acdb9f9f7',
+}
 
-  const config = {
-      reference: new Date().getTime(),
-      email: 'user@example.com',
-      amount: 20000,
-      publicKey: 'pk_test_dsdfghuytfd2345678gvxxxxxxxxxx',
-  }
+    const formattedPrice = price.toLocaleString()
+     const handlePaystackSuccessAction = (reference: any) => {
+         // Implementation for whatever you want to do with reference and after success call.
+         console.log(reference)
+     }
 
-  // you can call this function anything
-  const onSuccess = (reference:any) => {
-      // Implementation for whatever you want to do with reference and after success call.
-      console.log(reference)
-  }
+     // you can call this function anything
+     const handlePaystackCloseAction = () => {
+         // implementation for  whatever you want to do when the Paystack dialog closed.
+         console.log('closed')
+     }
 
-  // you can call this function anything
-  const onClose = () => {
-      // implementation for  whatever you want to do when the Paystack dialog closed.
-      console.log('closed')
-  }
+     const componentProps = {
+         ...config,
+         text: 'Paystack Button Implementation',
+         onSuccess: (reference:any) => handlePaystackSuccessAction(reference),
+         onClose: handlePaystackCloseAction,
+     }
 
-  const PaystackHookExample = () => {
-      const initializePayment = usePaystackPayment(config)
-      return (
-          <div>
-              <button
-                  onClick={() => {
-                      initializePayment(onSuccess, onClose)
-                  }}
-              >
-                  Paystack Hooks Implementation
-              </button>
-          </div>
-      )
-  }
+    return (
+        <div className='checkout'>
+            <h3 className='checkout__header'>Checkout</h3>
 
-  function App() {
-      return (
-          <div className='App'>
-              <header className='App-header'>
-                  <img src={logo} className='App-logo' alt='logo' />
-                  <p>
-                      Edit <code>src/App.js</code> and save to reload.
-                  </p>
-                  <a
-                      className='App-link'
-                      href='https://reactjs.org'
-                      target='_blank'
-                      rel='noopener noreferrer'
-                  >
-                      Learn React
-                  </a>
-              </header>
-              <PaystackHookExample />
-          </div>
-      )
-  }
+            <select className='checkout__select'>
+                <option value='localPay'>Local Deposit</option>
+                <option value='Paystack'>Card Payment</option>
+            </select>
 
-  export default App
+            <div className='checkout__paymentType'>
+                <h4>Account Details</h4>
+
+                <div className='checkout__paymentType--items'>
+                    <div>
+                        <span>Bank Name:</span>
+                        <p className='checkout__paymentType--item'>
+                            Gurantee Trust Bank
+                        </p>
+                    </div>
+                    <div>
+                        <span>Account Name:</span>
+                        <p className='checkout__paymentType--item'>
+                            Teens Can Blog Tech Comm.
+                        </p>
+                    </div>
+                    <div>
+                        <span>Account Number:</span>
+                        <p className='checkout__paymentType--item'>
+                            0632650272
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <div className='checkout__details'>
+                <h4>Summary</h4>
+
+                <div className='checkout__details--items'>
+                    <div>
+                        <span>Course:</span>
+                        <p className='checkout__details--item'>{title}</p>
+                    </div>
+                    <div>
+                        <span>Price:</span>
+                        <p className='checkout__details--item'>
+                            ₦{formattedPrice}{' '}
+                        </p>
+                    </div>
+                </div>
+
+                <button className='button checkout__btn'>
+                    <PaystackButton {...componentProps} />
+                </button>
+            </div>
+        </div>
+    )
+}
+
+export default Checkout
+
+
+
