296f386060a20863c0bbbf9b361520b87199c72b
Igboanugwo Collins <munisco12@gmail.com>
2021-06-29 08:28:35 +0100

    Added another course

--- src/views/Courses.tsx
+++ src/views/Courses.tsx
@@ -10,6 +10,7 @@ import course4 from '../assets/course4.jpg'
 import course5 from '../assets/course5.jpg'
 import course6 from '../assets/course6.jpg'
 import course7 from '../assets/course7.jpeg'
+import course8 from '../assets/course8.jpeg'
 
 const Courses = () => {
     const [courses, setCourses] = useState([])
